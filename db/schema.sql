a92608e0ee44c72eaad06493d642a427a0e8cdcf
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-08-06 12:23:25 -0600

    Created database with tables and keys

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department(
+  department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_id VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) 
+  REFERENCES department(department_id) 
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
